f15c5e210496efa361910126e78807fef63c4499
ivaneichenberger <ivaneichenberger@gmail.com>
2025-05-07 00:51:44 -0300

    hola lauti como estas yo bien

--- xdxd/src/App.js
+++ xdxd/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <h1>XD</h1>
+          <h1>Hola lauti como estas yo bien</h1>
       </header>
     </div>
   );
